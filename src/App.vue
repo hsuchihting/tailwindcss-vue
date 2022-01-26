09dccd9e2ee10fbded3d509d763c2436938aa88e
Tim.Hsu <tim.hsu@tpisoftware.com>
2022-01-26 17:30:50 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="py-4 bg-gray-400 text-white px-4 mx-auto">
-        <a class="px-4 py-4 cursor-pointer hover:bg-gray-600 duration-200"
+    <div class="py-4 bg-gray-500 text-white px-4 mx-auto">
+        <a class="px-4 py-4 cursor-pointer hover:bg-gray-700 duration-200"
             ><router-link to="/">home</router-link></a
         >
         <a class="px-4 py-4 cursor-pointer hover:bg-gray-600 duration-200">
             <router-link to="/card-demo">card demo</router-link></a
         >
     </div>
-    <div class="px-8 py-4">
+    <div class="px-8 py-4 ">
         <router-view></router-view>
     </div>
 </template>
